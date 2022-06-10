6a079fa1e16cea80ef79437add2dabdcc5f3d3f8
xiaodong zhu <72681553+zxd3099@users.noreply.github.com>
2022-06-10 08:09:46 +0800

    Update paper_portrait.py

--- RecommandCode/recprocess/portrait_process/paper_process/paper_portrait.py
+++ RecommandCode/recprocess/portrait_process/paper_process/paper_portrait.py
@@ -6,6 +6,9 @@
 @time: 2022-05-26 20:18
 """
 from dao.elastic_server import ElasticServer
+from conf.dao_config import paper_portrait_index_name
+from conf.dao_config import paper_portrait_doc_type
+import pandas as pd
 
 
 class PaperPortraitServer(object):
@@ -13,5 +16,53 @@ class PaperPortraitServer(object):
         """
         初始化
         """
-        self.elastic_server = ElasticServer()
+        self.es = ElasticServer().elastic_client
+        self.index_name = paper_portrait_index_name
+        self.doc_type = paper_portrait_doc_type
 
+    # noinspection PyMethodMayBeStatic
+    def paper_info_to_dataframe(self):
+        """
+        查询所有的论文，转换为DataFrame格式
+        :return:
+        """
+        body = {
+            "query": {
+                "term": {
+                    "new": 1
+                }
+            }
+        }
+        filter_path = ["hits.hits._source.key_words"]
+        query = self.es.search(index=self.index_name, doc_type=self.doc_type, filter_path=filter_path, body=body)
+
+        paper_info = pd.DataFrame(columns=("paper_id", "key_words"))
+        results = query["hits"]["hits"]  # es查询出的结果的第一页
+        total = query["hits"]["total"]  # es查询出的结果总量
+        scroll_id = query["_scroll_id"]  # 游标用于输出es查询出的所有结果
+
+        for i in range(0, int(total/100)+1):
+            query_scroll = self.es.scroll(scroll_id=scroll_id, scroll="5m")["hits"]["hits"]
+            results += query_scroll
+
+        for res in results:
+            paper_info.at[len(paper_info.index)] = [res["_id"], res["_source"]["key_words"]]
+        paper_info.set_index("paper_id", inplace=True)
+        return paper_info
+
+    def paper_to_subject(self, paper_id):
+        """
+        根据论文ID查找论文主题
+        :param paper_id:
+        :return:
+        """
+        body = {
+            "query": {
+                "term": {
+                    "paper_id": paper_id
+                }
+            }
+        }
+        filter_path = ["hits.hits._source.subject"]
+        query = self.es.search(index=self.index_name, doc_type=self.doc_type, filter_path=filter_path, body=body)
+        return query["hits"]["hits"]["_source"]["subject"]
