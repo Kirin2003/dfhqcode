4ccf02fe1b043d1e98bc5ad8816901ae207ef8fa
Candy <1940918445@qq.com>
2022-06-20 01:06:38 +0800

    页面1、2

--- FrontendCode/FrontendCode/src/router/index.js
+++ FrontendCode/FrontendCode/src/router/index.js
@@ -14,7 +14,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+      import(/* webpackChunkName: "about" */ "../views/UserInfoView.vue"),
+  },
+  {
+    path: "/User/:userName",
+    name: "User",
+    component: () => import("../views/UserInfoView.vue"),
   },
 ];
 
