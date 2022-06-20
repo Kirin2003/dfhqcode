b411f4c5de90f2d37e7167fc408d147d08da8b87
Candy <1940918445@qq.com>
2022-06-20 15:47:58 +0800

    页面1、2

---
+++ FrontendCode/frontcode/src/views/UserInfoView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="UserInfo">
+    <UserInfo />
+  </div>
+</template>
+<script>
+import UserInfo from "@/components/UserInfo.vue";
+export default {
+  name: "UserInfoView",
+  components: {
+    UserInfo,
+  },
+};
+</script>
