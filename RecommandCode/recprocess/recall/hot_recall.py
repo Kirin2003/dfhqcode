9ac7dfa0add37b207457dc78fa8ed070e72996a8
xiaodong zhu <72681553+zxd3099@users.noreply.github.com>
2022-06-21 11:43:38 +0800

    Update hot_recall.py

--- RecommandCode/recprocess/recall/hot_recall.py
+++ RecommandCode/recprocess/recall/hot_recall.py
@@ -5,19 +5,18 @@
 @file: hot_recall
 @time: 2022-05-29 15:42
 """
-from conf.dao_config import paper_portrait_index_name, paper_portrait_doc_type
+from conf.dao_config import DynamicInfo_index_name
 from dao.elastic_server import ElasticServer
 
 
 class HotRecall(object):
     def __init__(self):
         self.es = ElasticServer().elastic_client
-        self.index_name = paper_portrait_index_name
-        self.doc_type = paper_portrait_doc_type
+        self.index_name = DynamicInfo_index_name
 
-    def group_cate_for_paper_list(self, cate_dict):
+    def group_cate_for_paper_list_to_redis(self, cate_dict):
         """
-        按照类别输出论文推荐列表，并自动根据hot_value排序.
+        按照类别将论文推荐列表存入redis中，并自动根据hot_value排序.
         :param: cate_dict
         :return:
         """
@@ -29,14 +28,13 @@ class HotRecall(object):
             },
             "sort": [
                 {
-                    "hot_value": {
+                    "HotNum": {
                         "order": "desc"
                     }
                 }
             ]
         }
-        filter_path = ["hits.hits._source.paper_id",
-                       "hits.hits._source.hot_value"]
+        filter_path = ["hits.hits._source.Id"]
         # 初始化推荐列表
         user_rec = dict()
         for cate_id, cate_name in cate_dict.items():
@@ -44,15 +42,14 @@ class HotRecall(object):
 
         for cate_id, cate_name in cate_dict.items():
             body["query"]["term"]["subject"] = cate_name
-            query = self.es.search(index=self.index_name, doc_type=self.doc_type,
-                                    filter_path=filter_path, body=body)
+            query = self.es.search(index=self.index_name, filter_path=filter_path, body=body)
             results = query["hits"]["hits"]  # es查询结果的第一页
             total = query["hits"]["total"]  # es 查询出的结果总量
             scroll_id = query["_scroll_id"]  # 游标用于输出es查询的所有结果
             for i in range(0, int(total/100)+1):
                 query_scroll = self.es.scroll(scroll_id=scroll_id, scroll="5m")["hits"]["hits"]
-                user_rec[cate_name][results["_id"]] = results["_source"]["hot_value"]
+                user_rec[cate_name][results["_id"]] = results["_source"]["HotNum"]
                 results = query_scroll
-            user_rec[cate_name][results["_id"]] = results["_source"]["hot_value"]
+            user_rec[cate_name][results["_id"]] = results["_source"]["HotNum"]
 
         return user_rec
