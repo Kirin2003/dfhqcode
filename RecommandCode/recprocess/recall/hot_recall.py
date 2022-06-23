5e50c8c82b3570393d5e05742bd7380d3021c763
xiaodong zhu <72681553+zxd3099@users.noreply.github.com>
2022-06-23 17:06:40 +0800

    Update hot_recall.py

--- RecommandCode/recprocess/recall/hot_recall.py
+++ RecommandCode/recprocess/recall/hot_recall.py
@@ -14,7 +14,7 @@ class HotRecall(object):
         self.es = ElasticServer().elastic_client
         self.index_name = DynamicInfo_index_name
 
-    def group_cate_for_paper_list_to_redis(self, cate_dict):
+    def group_cate_for_paper_list(self, cate_dict):
         """
         按照类别将论文推荐列表存入redis中，并自动根据hot_value排序.
         :param: cate_dict
@@ -28,13 +28,12 @@ class HotRecall(object):
             },
             "sort": [
                 {
-                    "HotNum": {
+                    "hot_num": {
                         "order": "desc"
                     }
                 }
             ]
         }
-        filter_path = ["hits.hits._source.Id"]
         # 初始化推荐列表
         user_rec = dict()
         for cate_id, cate_name in cate_dict.items():
@@ -42,14 +41,10 @@ class HotRecall(object):
 
         for cate_id, cate_name in cate_dict.items():
             body["query"]["term"]["subject"] = cate_name
-            query = self.es.search(index=self.index_name, filter_path=filter_path, body=body)
-            results = query["hits"]["hits"]  # es查询结果的第一页
-            total = query["hits"]["total"]  # es 查询出的结果总量
-            scroll_id = query["_scroll_id"]  # 游标用于输出es查询的所有结果
-            for i in range(0, int(total/100)+1):
-                query_scroll = self.es.scroll(scroll_id=scroll_id, scroll="5m")["hits"]["hits"]
-                user_rec[cate_name][results["_id"]] = results["_source"]["HotNum"]
-                results = query_scroll
-            user_rec[cate_name][results["_id"]] = results["_source"]["HotNum"]
+            query = self.es.search(index=self.index_name, body=body)["hits"]
+            total = query["total"]["value"]
+            result = query["hits"]
+            for i in range(0, total):
+                user_rec[cate_name][result[i]["_id"]] = result[i]["_source"]["hot_num"]
 
         return user_rec
