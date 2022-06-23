eb020ccb61713fe390744bd72310afb47c49fd68
xiaodong zhu <72681553+zxd3099@users.noreply.github.com>
2022-06-23 16:59:51 +0800

    Update dao_config.py

--- RecommandCode/conf/dao_config.py
+++ RecommandCode/conf/dao_config.py
@@ -8,30 +8,30 @@
 # MySQL配置
 mysql_username = "root"
 mysql_passwd = "123456"
-mysql_hostname = "127.0.0.1"
+mysql_hostname = "localhost"
 mysql_port = "3306"
 
 # MySQL数据库
 user_info_db_name = "userinfo"  # 用户数据相关的数据库
 register_user_table_name = "register_user"  # 注册用户数据表
-user_collections_table_name = "user_collections"  # 用户收藏数据表
+user_collections_table_name = "user_collection"  # 用户收藏数据表
 user_read_table_name = "user_read"   # 用户阅读数据表
-user_likes_table_name = "user_likes"  # 用户点赞数据表
+user_likes_table_name = "user_like"  # 用户点赞数据表
 
 # ElasticSearch配置
 elastic_hostname = "127.0.0.1"
 elastic_username = "elastic"
-elastic_passwd = "123456"
+elastic_passwd = "JSAEtbtfzVrlJpNy6eEP"
 elastic_port = 9200
 paper_index_name = "paper"
-DynamicInfo_index_name = "DynamicInfo"
+DynamicInfo_index_name = "dynamic_info"
 
 # 论文的分类
 cate_dict = {
     "1": "Computer vision and Pattern recognition",
     "2": "Machine Learning",
     "3": "Robotics",
-    "4": "Image and Video processing ",
+    "4": "Image and Video processing",
     "5": "Artificial Intelligence",
     "6": "Others"
 }
