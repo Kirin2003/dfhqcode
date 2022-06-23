4845a11119813abf1323c2d361558fb7b08f74c0
xiaodong zhu <72681553+zxd3099@users.noreply.github.com>
2022-06-23 17:06:14 +0800

    Update user_portrait.py

--- RecommandCode/recprocess/portrait_process/user_process/user_portrait.py
+++ RecommandCode/recprocess/portrait_process/user_process/user_portrait.py
@@ -6,7 +6,7 @@
 @time: 2022-05-26 22:34
 """
 from collections import Counter, defaultdict
-from datetime import datetime
+from datetime import datetime as dt, timedelta
 
 import pandas as pd
 
@@ -70,8 +70,8 @@ class UserPortraitServer(object):
         table_obj, history = None, None
         feature_dict = defaultdict(dict)
 
-        end = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        start = (datetime.now() + datetime.timedelta(days=-time_range)).strftime("%Y-%m-%d %H:%M:%S")
+        end = dt.now().strftime("%Y-%m-%d %H:%M:%S")
+        start = (dt.now() + timedelta(days=-time_range)).strftime("%Y-%m-%d %H:%M:%S")
 
         for type in behavior_types:
             if type == "read":
@@ -82,7 +82,7 @@ class UserPortraitServer(object):
                 table_obj = UserCollections
             try:
                 history = self.user_sess.query(table_obj)\
-                                        .filter(table_obj.userid == user_id)\
+                                        .filter(table_obj.user_id == user_id)\
                                         .filter(table_obj.time >= start)\
                                         .filter(table_obj.time <= end)\
                                         .all()
@@ -109,17 +109,10 @@ class UserPortraitServer(object):
         for h in history:
             paper_id = h.paper_id
             history_paper_id.append(paper_id)
-            body = {
-                "query": {
-                    "term": {
-                        "Id": paper_id
-                    }
-                }
-            }
-            result = self.es.search(index=self.index_name, body=body)
+            result = self.es.get(index=self.index_name, id=paper_id)
             # 这里需要根据字段名来修改
-            history_paper_cate.append(result["hits"]["hits"]["_source"]["cate"])
-            history_keyword += result["hits"]["hits"]["_source"]["key_words"].split(",")
+            history_paper_cate.append(result["_source"]["subject"])
+            history_keyword += result["_source"]["paperKeywords"]
 
         feature_dict = dict()
 
@@ -161,5 +154,5 @@ class UserPortraitServer(object):
                 df.loc[(df["user_id"] == collection.user_id) & (df["paper_id"] == collection.paper_id), "collection"] = 1
             else:
                 df.at[len(df.index)] = [collection.paper_id, collection.user_id, 0, 1, 0, collection.time]
-        df.sort_values("time", inplace=True)
+        df.sort_values("Time", inplace=True)
         return df
