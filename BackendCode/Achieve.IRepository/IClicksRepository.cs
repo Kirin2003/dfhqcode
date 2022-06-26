f9d1bcdf1cc1d139c9a8fcb8fbdde6a4382f2410
hxq <1405871856@qq.com>
2022-06-26 16:20:22 +0800

    后端代码整合1

--- BackendCode/Achieve.IRepository/IClicksRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Achieve.IRepository.Base;
-using Achieve.Model.EntityModels;
-
-namespace Achieve.IRepository
-{
-    public partial interface IClicksRepository : IBaseRepository<Clicks>
-    {
-    }
-}
